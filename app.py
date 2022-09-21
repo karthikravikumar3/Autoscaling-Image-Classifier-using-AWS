c453c7db91eb07e8d87f84ebec0db4f0b4605673
karthikravikumar6 <113553044+karthikravikumar6@users.noreply.github.com>
2022-09-20 18:54:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ from flask import Flask, flash, request
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/ubuntu/savedImages'
-app = Flask(__name__)
+app = Flask(_name_)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 sqs=boto3.client('sqs')
+
+request_queue_url = 'https://sqs.us-east-1.amazonaws.com/477824770261/RequestQueue'
+response_queue_url = 'https://sqs.us-east-1.amazonaws.com/477824770261/ResponseQueue'
 
 @app.route('/')
 def hello_world():
@@ -23,12 +26,11 @@ def upload():
             file2.save(path)
             with open("/home/ubuntu/savedImages/"+filename, "rb") as image2string:
                 bytes = base64.b64encode(image2string.read())
-                sqs.send_message(QueueUrl='https://sqs.us-east2.amazonaws.com/321247833586/RequestQueue',
+                sqs.send_message(QueueUrl=request_queue_url,
                         MessageAttributes={
         'ImageName': {
             'DataType': 'String',
             'StringValue': filename
         }},MessageBody=bytes.decode('ascii'))
-                print('filename')
+                print(filename)
         return 'hi'
-
